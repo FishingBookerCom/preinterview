c08c6e13e71462cb1abe153a51d7f03777c9e844
Mikica Ivosevic <mikica.ivosevic@gmail.com>
2015-03-02 16:13:20 +0100

    fix check_brackets

--- problem2.php
+++ problem2.php
@@ -1,8 +1,9 @@
 /* Implementiraj funkciju koja proverava da li su zagrade unutar datog stringa ispravno uparene. */
 
 function check_brackets($str) {
-    $brackets = array('{' => '}', '(' => ')', '[' => ']');
+    $brackets = array('(' => ')', '{' => '}', '[' => ']');
     $stackArray = array();
+    $str = preg_replace("/[a-zA-Z0-9]/", "", $str);
     $strLength = strlen($str);
     if($str != '' && $strLength >= 2){
         for ($i=0; $i < $strLength; $i++) {

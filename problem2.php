c29e497949cc7d67b40131afbd69e14fa40164db
Mikica Ivosevic <mikica.ivosevic@gmail.com>
2015-03-02 15:45:16 +0100

    update check_brackets method

--- problem2.php
+++ problem2.php
@@ -1,7 +1,20 @@
 /* Implementiraj funkciju koja proverava da li su zagrade unutar datog stringa ispravno uparene. */
 
 function check_brackets($str) {
-    return FALSE;
+    $brackets = array('{' => '}', '(' => ')', '[' => ']');
+    $stackArray = array();
+    $strLength = strlen($str);
+    if($str != '' && $strLength >= 2){
+        for ($i=0; $i < $strLength; $i++) {
+            $currentStr = $str[$i];
+            if($brackets[end($stackArray)] == $currentStr){
+                array_pop($stackArray);
+            }else{
+                array_push($stackArray, $currentStr);
+            }
+        }
+    }
+    return !count($stackArray);
 }
 
 assert(check_brackets('asd') === TRUE);

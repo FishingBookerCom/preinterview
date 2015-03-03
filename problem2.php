3eed3994564f0c485cbbc4875bab993af3ba1426
Grujah <grujah@gmail.com>
2015-03-03 16:16:14 +0100

    Update problem2.php

--- problem2.php
+++ problem2.php
@@ -1,7 +1,24 @@
 /* Implementiraj funkciju koja proverava da li su zagrade unutar datog stringa ispravno uparene. */
 
 function check_brackets($str) {
-    return FALSE;
+    $len = strlen($str);
+    $stack = array();
+    for ($i = 0; $i < $len; $i++) {
+        switch ($str[$i]) {
+			case '(':
+			case '[':
+			case '{':
+            case '<': array_push($stack, $str[$i]); break;			
+            case ')': if (array_pop($stack) !== '(') return FALSE;  break;
+            case ']': if (array_pop($stack) !== '[') return FALSE;  break;
+			case '}': if (array_pop($stack) !== '{') return FALSE;  break;
+            case '>': if (array_pop($stack) !== '<') return FALSE;  break;
+ 
+           
+            default: break;
+        }
+    }
+    return empty($stack);
 }
 
 assert(check_brackets('asd') === TRUE);

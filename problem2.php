4597fa1c5ae68ac573f7dd17cfb02cb8336e4ed6
Valentin1994 <hisari.valentin@gmail.com>
2016-01-13 18:22:42 +0100

    Update problem2.php

--- problem2.php
+++ problem2.php
@@ -1,7 +1,25 @@
 /* Implementiraj funkciju koja proverava da li su zagrade unutar datog stringa ispravno uparene. */
 
 function check_brackets($str) {
-    return FALSE;
+	$bracket0 = 0;
+	$bracket1 = 0;
+	$bracket2 = 0;
+	for($i=0; $i<strlen($str); $i++){
+		if($str[$i]=='(')
+			$bracket0++;
+		if($str[$i]==')')
+			$bracket0--;
+		if($str[$i]=='[')
+			$bracket1++;
+		if($str[$i]==']')
+			$bracket1--;
+		if($str[$i]=='{')
+			$bracket2++;
+		if($str[$i]=='}')
+			$bracket2--;
+	}
+	$ret = $bracket0==0 && $bracket1==0 && $bracket2==0;
+    return $ret;
 }
 
 assert(check_brackets('asd') === TRUE);
